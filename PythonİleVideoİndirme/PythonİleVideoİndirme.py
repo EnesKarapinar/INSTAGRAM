a57614024623029d6d76ce25b224b1f4b2741965
EnesKarapinar <karapinarabdullahenes@gmail.com>
2021-09-11 15:42:21 +0300

    PythonİleVideoİndirme

---
+++ PythonİleVideoİndirme/PythonİleVideoİndirme.py
@@ -0,0 +1,10 @@
+# Kütüphanemizi yüklemek için --> pip install youtube_dl
+
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=0eVva0ceeFI'])
+                            
